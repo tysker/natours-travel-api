094ed561511e9ed516aad61d306675b35d6cd9ef
Jörg Oertel <tyskerjoerg@gmail.com>
2022-11-22 12:51:24 +0100

    v106-v107

--- controllers/controller.js
+++
@@ -1,8 +0,0 @@
-const employee = {
-  name: 'John Smith',
-  position: 'Sales Manager'
-};
-const { position, ...employeeRest } = employee;
-console.log(employeeRest); // { name: 'John Smith' }
-console.log(employee);
-console.log(position);
